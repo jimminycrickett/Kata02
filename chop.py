ded805ddebdc2a6be0f9400ebffa9e00d50dff6e
jimminycrickett <jimminycrickett@protonmail.com>
2018-03-25 09:47:40 -0600

    Kata02 chop3 code

--- chop.py
+++ chop.py
@@ -42,6 +42,27 @@ def chop2_test():
     assert chop2(8, [1, 3, 5, 7])== -1
     return "chop2 Passed!"
 
+def chop3_test():
+    assert chop3(3,[]) == -1
+    assert chop3(3,[1]) == -1
+    assert chop3(1,[1]) == 0
+    assert chop3(1, [1, 3, 5]) == 0
+    assert chop3(3, [1, 3, 5]) == 1
+    assert chop3(5, [1, 3, 5]) == 2
+    assert chop3(0, [1, 3, 5]) == -1
+    assert chop3(2, [1, 3, 5]) == -1
+    assert chop3(4, [1, 3, 5]) == -1
+    assert chop3(6, [1, 3, 5]) == -1
+    assert chop3(1, [1, 3, 5, 7]) == 0
+    assert chop3(3, [1, 3, 5, 7]) == 1
+    assert chop3(5, [1, 3, 5, 7]) == 2
+    assert chop3(7, [1, 3, 5, 7]) == 3
+    assert chop3(0, [1, 3, 5, 7])== -1
+    assert chop3(2, [1, 3, 5, 7])== -1
+    assert chop3(4, [1, 3, 5, 7])== -1
+    assert chop3(6, [1, 3, 5, 7])== -1
+    assert chop3(8, [1, 3, 5, 7])== -1
+    return "chop3 Passed!"
 
 def chop(int, list_of_int):
     if type(int).__name__ != 'int':
@@ -55,9 +76,18 @@ def chop(int, list_of_int):
 
 chop2 = lambda int, list_of_int: list_of_int.index(int) if int in list_of_int else -1
 
+def chop3(integer, list_of_int):
+    result = -1
+    for index in range(len(list_of_int)):
+        if list_of_int[index] == integer:
+            result = index
+            break
+        else:
+            continue
+    return result
 
 print(chop_test())
 print(chop2_test())
-
+print(chop3_test())
 
 
